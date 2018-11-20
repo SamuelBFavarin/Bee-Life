9a1249ba9677858c564be25bd6f138f1004510fc
Vinícius Adriano Machado <viniciusadrianomachado@gmail.com>
2018-11-20 00:05:35 -0200

    Inicio dos novos behaviour

---
+++ BeeLife/src/model_agents/bee/BeeInHive.java
@@ -0,0 +1,21 @@
+package model_agents.bee;
+
+import jade.core.Agent;
+import jade.core.behaviours.TickerBehaviour;
+
+/**
+ *
+ * @author Vinicius
+ */
+public class BeeInHive extends TickerBehaviour {
+
+    public BeeInHive(Agent a, long period) {
+        super(a, period);
+    }
+
+    @Override
+    protected void onTick() {
+        
+    }
+    
+}
